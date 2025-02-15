8a858e2e7280e4c77f341b1df6d9aa0999aaa2b5
akshat <akshat@dentira.com>
2025-02-15 16:17:48 +0530

    debug

--- ssl_seizure_detection/src/data/preprocess.py
+++ ssl_seizure_detection/src/data/preprocess.py
@@ -746,7 +746,6 @@ def run_sorter(logdir,leave_index, run_type="all"):
                         test_runs.append(torch.load(os.path.join(dir_path, run), weights_only=False))
                     else:
                         all_train_runs.append(torch.load(os.path.join(dir_path, run), weights_only=False))
-                    index_stop+=1
             index += 1
         return all_train_runs,test_runs
     else:
